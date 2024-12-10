73c37660f968c3695e87623f903c885516cf62a0
Dylan Gregory <dylanjgregory@icloud.com>
2024-12-10 19:53:06 +0100

    Add Dockerfile for frontend; update compose.yaml to run frontend and backend together

--- backend/server/main.py
+++ backend/server/main.py
@@ -51,7 +51,7 @@ app = FastAPI()
 # Configure CORS to allow requests from specific origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
