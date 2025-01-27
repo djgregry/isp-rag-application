e425859958892ee8bc8a8a37eadb3c1c9d79bf21
Dylan Gregory <dylanjgregory@icloud.com>
2025-01-27 22:41:40 +0100

    Enhance batching logic for fetching and adding PubMed articles

--- backend/loaders/pubmed.py
+++ backend/loaders/pubmed.py
@@ -5,6 +5,8 @@ from typing import Dict, List
 import weaviate
 from dotenv import load_dotenv
 from Bio import Entrez
+from datetime import datetime, timedelta
+from sentence_transformers import SentenceTransformer
 from weaviate.classes.config import Configure, Property, DataType
 
 # Load environment variables
@@ -22,53 +24,82 @@ Entrez.email = NCBI_EMAIL
 Entrez.api_key = NCBI_API_KEY
 
 
-def fetch_pubmed_articles(query: str, retmax:int=100) -> List[Dict[str,str]]:
+# Solution to limited results on Entrex esearch
+def split_date_ranges(start_date: str, end_date: str, days_per_range: int) -> List[Dict[str, str]]:
+    date_ranges = []
+    current_start = datetime.strptime(start_date, "%Y/%m/%d")
+    current_end = min(current_start+timedelta(days=days_per_range), datetime.strptime(end_date,"%Y/%m/%d"))
+
+    # Iterate through the date range, creating smaller chunks
+    while current_start < datetime.strptime(end_date, "%Y/%m/%d"):
+        date_ranges.append({
+            "start": current_start.strftime("%Y/%m/%d"),
+            "end": current_end.strftime("%Y/%m/%d")
+        })
+        current_start = current_end + timedelta(days=1)
+        current_end = min(current_start+timedelta(days=days_per_range), datetime.strptime(end_date,"%Y/%m/%d"))
+
+    return date_ranges
+
+
+def search_pubmed_in_range(query:str, start_date:str = "", end_date:str = ""):
+    # Split date ranges for pagination
+    try:
+        ids = []
+        date_ranges = split_date_ranges(start_date, end_date, days_per_range=100)
+
+        for date_range in date_ranges:
+            print(f"Fetching articles published {date_range['start']}-{date_range['end']}")
+            with Entrez.esearch(
+                db="pubmed", 
+                term=query, 
+                retmax=9999,
+                mindate=date_range['start'],
+                maxdate=date_range['end']
+            ) as search_result:
+                record =  Entrez.read(search_result)
+                ids.extend(record.get("IdList", []))
+        
+        return ids
+    
+    except Exception as e:
+        print(f"Error searching articles: {e}")
+        return []
+
+
+def fetch_pubmed_articles(ids: List[str]) -> List[Dict[str, str]]:
     """
-    Fetch PubMed articles based on a query.
+    Fetch and structuring PubMed articles given a list of Article IDs
 
     Args:
-        query (str): The search query for PubMed.
-        retmax (int): Maximum number of articles to return.
-    
+        ids (List[str]): List of Articles IDs given from PubMed Database
+
     Returns:
         List[Dict[str,str]]: A list of articles with abstract and URL.
     """
-    try:
-        search_result = Entrez.esearch(db="pubmed", term=query, retmax=retmax)
-        record = Entrez.read(search_result)
-        ids = record.get("IdList", [])
-        
+    with Entrez.efetch(db="pubmed", id=",".join(ids), rettype="gb", retmode="xml") as fetch_id:
         articles = list()
-        for article_id in ids:
-            # Fetch details of an article given its ID
-            fetch_id = Entrez.efetch(db="pubmed", id=article_id, rettype="gb", retmode="xml")
-            records = Entrez.read(fetch_id)
+        records = Entrez.read(fetch_id)
 
-            for record in records.get("PubmedArticle", []):
-                article = record['MedlineCitation']['Article']
+        for record in records.get("PubmedArticle", []):
+            article = record['MedlineCitation']['Article']
+            article_id = record['MedlineCitation']['PMID']
 
-                # Extract article details
-                title = article.get("ArticleTitle", "Title Not Available")
-                abstract_sections = article.get("Abstract", {}).get("AbstractText", [])
-                abstract = "\n\n".join(
-                    f"## {section.attributes.get('Label', 'SECTION')}\n\n{section}"
-                    for section in abstract_sections
-                )
+            # Extract article details
+            title = article.get("ArticleTitle", "Title Not Available")
+            abstract_sections = article.get("Abstract", {}).get("AbstractText", [])
+            abstract = "\n\n".join(
+                f"## {section.attributes.get('Label', 'SECTION')}\n\n{section}"
+                for section in abstract_sections
+            )
 
-                # Add article detials to the list
-                articles.append({
-                    "abstract": f"## TITLE\n\n{title}\n\n{abstract}",
-                    "url": f"{PUBMED_BASE_URL}{article_id}"
-                })
-
-                # Avoid API rate limits
-                time.sleep(0.1)
+            # Add article detials to the list
+            articles.append({
+                "abstract": f"## TITLE\n\n{title}\n\n{abstract}",
+                "url": f"{PUBMED_BASE_URL}{article_id}"
+            })
         
         return articles
-    
-    except Exception as e:
-        print(f"Error fetching articles: {e}")
-        return []
 
 
 def setup_weaviate(client: weaviate.Client):
@@ -81,9 +112,7 @@ def setup_weaviate(client: weaviate.Client):
     if not client.collections.exists("Articles"):
         client.collections.create(
             "Articles",
-            vectorizer_config = Configure.Vectorizer.text2vec_huggingface(
-                model="sentence-transformers/all-MiniLM-L6-v2",
-            ),
+            vectorizer_config = Configure.Vectorizer.none(),
             properties=[
                 Property(name="abstract", data_type=DataType.TEXT),
                 Property(name="url", data_type=DataType.TEXT, skip_vectorization=True)
@@ -92,16 +121,30 @@ def setup_weaviate(client: weaviate.Client):
         print("Collection created successfully.")
 
 
-def add_data(client: weaviate.Client, collection: str):
+def add_data(client: weaviate.Client, query: str):
     """
     Add data to the Weaviate collection.
     """
     setup_weaviate(client)
-    articles = fetch_pubmed_articles("Alzheimer's Disease", retmax=100000)
-    collection = client.collections.get(collection)
+    collection = client.collections.get("Articles")
+    refined_query = f"{query} AND hasabstract[text] AND free full text[sb]"
+    ids = search_pubmed_in_range(refined_query, "2000/01/01", "2025/03/01")
+    model = SentenceTransformer("neuml/pubmedbert-base-embeddings")
+
+    # Use batch processing for efficient insertion of articles into the Weaviate collection
     with collection.batch.dynamic() as batch:
-        for article in articles:
-            batch.add_object(properties=article)
+        batch_size = 500
+        for i in range(0, len(ids), batch_size):
+            fetch_ids = ids[i:i+batch_size]
+            articles = fetch_pubmed_articles(fetch_ids)
+
+            for article in articles:
+                embedding = model.encode(article["abstract"]) 
+                batch.add_object(
+                    properties=article,
+                    vector=embedding.tolist()
+                )
+
     print("Articles added successfully to Weaviate.")
 
 
@@ -125,8 +168,9 @@ def delete_all_collections(client: weaviate.Client):
 
 
 def main():
+
     parser = argparse.ArgumentParser(description="Weaviate Operations Script")
-    parser.add_argument("--add-data", type=str, help="Add data to the specified collection")
+    parser.add_argument("--add-data", type=str, help="Add data from PubMed with respect to a query")
     parser.add_argument("--delete-collection", type=str, help="Delete a specific collection")
     parser.add_argument("--delete-all", action="store_true", help="Delete all collections")
 
@@ -146,6 +190,7 @@ def main():
     
     except Exception as e:
         print(f"Error in main execution: {e}")
+
     finally:
         client.close()
 
